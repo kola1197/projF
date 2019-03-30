e6c0b5eebc1870e3c8cf1b393c5e5e4bf7d06b0d
kola1197 <kola-1197@mail.ru>
2019-03-31 02:47:15 +0300

    test

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 lastPosition;
+    private Vector3 delta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = target.position;
+        if (target == null) Debug.LogError("Target for camera isn't set!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta = target.position - lastPosition;
+    }
+
+    void LateUpdate()
+    {
+        transform.position += delta;
+        lastPosition = target.position;
+    }
+}
---
+++ Assets/Scripts/TreeFilter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TreeFilter : MonoBehaviour
+{
+    public string treeTag = "Tree";
+
+    private CameraFollowScript cameraFollow;
+    private Vector3 worldSpacePoint;
+    private Ray ray;
+    private RaycastHit hit;
+    private Camera mainCamera;
+    private MeshRenderer lastTree, newTree;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        cameraFollow = mainCamera.GetComponent<CameraFollowScript>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        worldSpacePoint = mainCamera.ViewportToWorldPoint(new Vector3(0.5F, 0.5F, 0));
+        Physics.Raycast(worldSpacePoint, (cameraFollow.target.position - worldSpacePoint), out hit);
+        if(hit.collider.tag == treeTag)
+        {
+            Debug.Log("I see a tree");
+            newTree = hit.collider.gameObject.GetComponent<MeshRenderer>();
+            Debug.Log(lastTree == null);
+            if ((lastTree == null) || (lastTree != newTree))
+            {
+                if (lastTree != null) lastTree.enabled = true;
+                lastTree = newTree;
+                lastTree.enabled = false;
+            }
+        }
+        else
+        {
+            if (lastTree != null)
+            {
+                lastTree.enabled = true;
+                lastTree = null;
+            }
+        }
+
+    }
+}
